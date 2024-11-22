ceec0a00fd1f35d2871511dc9d9d246b49dd2c54
Oliver With <oliverwith@bbv.ch>
2024-11-22 15:24:29 +0100

    Work with rust object from C++
    It's awful as we need wrapper functions and raw pointers

--- cpp-project/main.cpp
+++ cpp-project/main.cpp
@@ -6,10 +6,16 @@ extern "C" {
 #include "rust_c_bindings.h"
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     std::cout << "cpp_project::getValue() => " << cpp_project::getValue() << std::endl;
     std::cout << "[extern] rust_function() => " << rust_function() << std::endl;
+
+    SuperStructure* ss = super_structure_new();
+    super_structure_increment(ss);
+    size_t counter = super_structure_get_counter(ss);
+    super_structure_free(ss);
+
+    std::cout << "Worked with SuperStructure: counter: " << counter << std::endl;
 
     return EXIT_SUCCESS;
 }
--- rust-project/src/lib.rs
+++ rust-project/src/lib.rs
@@ -2,3 +2,64 @@
 pub extern "C" fn rust_function() -> u64 {
     123
 }
+
+#[repr(C)]
+pub struct SuperStructure {
+    counter: usize,
+}
+
+impl SuperStructure {
+    pub fn new() -> SuperStructure {
+        println!("New SuperStructure created.");
+        SuperStructure { counter: 0 }
+    }
+
+    pub fn increment(&mut self) {
+        self.counter += 1;
+    }
+
+    pub fn get_counter(&self) -> usize {
+        self.counter
+    }
+}
+
+impl Drop for SuperStructure {
+    fn drop(&mut self) {
+        println!(
+            "SuperStructure with counter {} is being dropped.",
+            self.counter
+        );
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn super_structure_new() -> *mut SuperStructure {
+    Box::into_raw(Box::new(SuperStructure::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn super_structure_increment(ptr: *mut SuperStructure) {
+    if !ptr.is_null() {
+        unsafe {
+            (*ptr).increment();
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn super_structure_get_counter(ptr: *const SuperStructure) -> usize {
+    if ptr.is_null() {
+        0
+    } else {
+        unsafe { (*ptr).get_counter() }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn super_structure_free(ptr: *mut SuperStructure) {
+    if !ptr.is_null() {
+        unsafe {
+            drop(Box::from_raw(ptr));
+        }
+    }
+}
